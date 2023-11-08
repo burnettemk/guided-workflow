fc5c800f3e3a5892b2841fa1b232ec32f342edb4
burnettemk <wafflesxyz@gmail.com>
2023-11-08 11:45:16 -0800

    Added program loop and renamed variables

--- start.py
+++ start.py
@@ -18,9 +18,9 @@ def create_workspace():
   generate_bat(workspace_name)
 
 
-def run_subprocess(process, cwd = "", workspace = ""):
+def run_subprocess(process, cwd = "", path = ""):
   if process == "explorer":
-    return_code = subprocess.call(['start', 'C:/Windows/explorer', workspace], shell=True)
+    return_code = subprocess.call(['start', 'C:/Windows/explorer', path], shell=True)
     if return_code == 0:
       print("Command executed successfully.")
     else:
@@ -28,7 +28,7 @@ def run_subprocess(process, cwd = "", workspace = ""):
   elif process == "cmd":
     os.chdir('data')
 
-    return_code = subprocess.call([workspace + '.bat'], shell=True)
+    return_code = subprocess.call([path + '.bat'], shell=True)
     if return_code == 0:
       print("Command executed successfully.")
     else:
@@ -60,33 +60,41 @@ def generate_bat(wrkspc_name: str):
 
 def start_workspace(wrkspc_name : str):
   # Check for folder existence. If it exists, open file explorer in that folder and run created .bat file
-  file_path = 'data/' + workspace + '.bat'
+  file_path = 'data/' + wrkspc_name + '.bat'
 
   if exists(file_path):
 
     # Call start.bat to run the workspace
     print("Starting workspace...")
 
-    run_subprocess("cmd", cwd, workspace)
+    run_subprocess("cmd", cwd, path=wrkspc_name)
 
     # Find directory
     os.chdir('C:/Users/waffl/Workspaces')
 
-    if exists(workspace):
+    if exists(wrkspc_name):
       print("Opening workspace folder...")
-      run_subprocess("explorer", workspace)
+      run_subprocess("explorer", path=wrkspc_name)
     else:
       print("Creating workspace folder...")
-      os.mkdir(workspace)
-      run_subprocess("explorer", workspace)
+      os.mkdir(wrkspc_name)
+      run_subprocess("explorer", path=wrkspc_name)
 
     os.chdir(cwd)
   else:
     print("Associated .bat or .cmd file not found.\nGenerating new .bat file...")
-    generate_bat(workspace)
+    generate_bat(wrkspc_name)
 
-    start_workspace(workspace)
+    start_workspace(wrkspc_name)
 
+
+def print_workspaces():
+  print("Available Workspaces: ")
+  f = open("data/workspaces.txt", "r")
+  for x in f:
+    workspaces.append(x.replace('\n', ''))
+    print(x)
+  f.close
 
 
 if __name__ == "__main__":
@@ -99,12 +107,7 @@ if __name__ == "__main__":
     os.mkdir("data")
 
   if exists("data/workspaces.txt"):
-    print("Available Workspaces: ")
-    f = open("data/workspaces.txt", "r")
-    for x in f:
-      workspaces.append(x.replace('\n', ''))
-      print(x)
-    f.close
+    print_workspaces()
   else:
     print("No workspaces found. Would you like to make one? Y/N")
     res = input()
@@ -133,3 +136,8 @@ if __name__ == "__main__":
       create_workspace()
       start_workspace(workspace)
 
+  name = input("\nInput name of workspace to start it or input exit to quit the program: ")
+  while name != 'exit':
+    start_workspace(name)
+    print_workspaces()
+    name = input("\nInput name of workspace to start it or input exit to quit the program: ")
